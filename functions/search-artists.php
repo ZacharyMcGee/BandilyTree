c657b11d1bc72e41c5ea519e8897177e46438510
Zachary McGee <zachary.mcgee@siu.edu>
2019-11-21 12:29:52 -0600

    Create keys.php

---
+++ functions/search-artists.php
@@ -0,0 +1,31 @@
+<?php
+include 'keys.php';
+
+$artist = 1;
+
+if (isset($_GET['artist']))
+{
+    $artist = $_GET['artist'];
+}
+else
+{
+    // Fallback behaviour goes here
+}
+
+$ch = curl_init('https://api.discogs.com/database/search?q=' . $artist . '&type=artist');
+
+curl_setopt(
+    $ch,
+    CURLOPT_HTTPHEADER,
+    array(
+        'Content-Type: application/x-www-form-urlencoded',
+        'Authorization: Discogs key=' . $key . ', secret=' . $secret . '',
+        'User-Agent: VinylAnalysis'
+    )
+);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+curl_getinfo($ch);
+echo curl_exec($ch);
+curl_close($ch);
+?>
